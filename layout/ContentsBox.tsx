70474e6a0ba96defa8b6813ddd517e3b60c1d978
crucial-sub <jssub940@gmail.com>
2022-06-09 23:12:08 +0900

    [Feat] 콘테이너, 콘텐츠박스 레이아웃

---
+++ layout/ContentsBox.tsx
@@ -0,0 +1,7 @@
+import { GlobalLayoutProps } from 'types/GlobalLayoutProps';
+
+const ContentsBox = ({ children }: GlobalLayoutProps) => {
+    return <div>{children}</div>;
+};
+
+export default ContentsBox;
