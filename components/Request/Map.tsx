48c00b96d8cf4d7c33f85d77934c3a3738838b80
crucial-sub <jssub940@gmail.com>
2022-06-20 14:00:09 +0900

    [Feat] 카카오맵 지도에서 클릭 시 해당 죄표로 마커를 이동시켜 원하는 호출 위치 지정 기능 구현

--- components/Request/Map.tsx
+++ components/Request/Map.tsx
@@ -15,32 +15,63 @@ declare global {
 
 const Map = (props: Props) => {
     const { lat, lng } = props;
+    const [centerPosition, setCenterPosition] = useState({
+        lat: lat,
+        lng: lng,
+    });
+    const [clickPosition, setClickPosition] = useState({
+        lat: lat,
+        lng: lng,
+    });
+    const [mapLevel, setMapLevel] = useState(3);
     useEffect(() => {
-        const mapScript = document.createElement('script');
-
-        mapScript.async = true;
-        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=287ffecf2171baeaf8539863aa7ad6c1&autoload=false`;
-
-        document.head.appendChild(mapScript);
-
         const onLoadKakaoMap = () => {
             window.kakao.maps.load(() => {
-                const container = document.getElementById('map');
-                const options = {
-                    center: new window.kakao.maps.LatLng(lat, lng),
+                const mapContainer = document.getElementById('map');
+                const mapOption = {
+                    center: new window.kakao.maps.LatLng(
+                        centerPosition.lat,
+                        centerPosition.lng,
+                    ),
+                    level: mapLevel,
                 };
-                const map = new window.kakao.maps.Map(container, options);
-                const markerPosition = new window.kakao.maps.LatLng(lat, lng);
+
+                const map = new window.kakao.maps.Map(mapContainer, mapOption);
                 const marker = new window.kakao.maps.Marker({
-                    position: markerPosition,
+                    position: new window.kakao.maps.LatLng(
+                        clickPosition.lat,
+                        clickPosition.lng,
+                    ),
                 });
                 marker.setMap(map);
+
+                window.kakao.maps.event.addListener(
+                    map,
+                    'click',
+                    (mouseEvent: any) => {
+                        const level = map.getLevel();
+                        setMapLevel(() => level);
+
+                        const mapCenter = map.getCenter();
+                        setCenterPosition({
+                            ...centerPosition,
+                            lat: mapCenter.getLat(),
+                            lng: mapCenter.getLng(),
+                        });
+
+                        const clickLatLng = mouseEvent.latLng;
+                        setClickPosition({
+                            ...clickPosition,
+                            lat: clickLatLng.getLat(),
+                            lng: clickLatLng.getLng(),
+                        });
+                        marker.setPosition(clickLatLng);
+                    },
+                );
             });
         };
-        mapScript.addEventListener('load', onLoadKakaoMap);
-
-        return () => mapScript.removeEventListener('load', onLoadKakaoMap);
-    }, [lat, lng]);
+        return onLoadKakaoMap();
+    }, [centerPosition, clickPosition]);
 
     return <MapContainer id="map" />;
 };
