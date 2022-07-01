68dffc4b4f595c4c41277942d9e732ff723cc134
crucial-sub <jssub940@gmail.com>
2022-07-01 23:47:54 +0900

    [Fix] 하단 탭이 특정상황에서 제대로 작동 안하는 오류 해결

--- components/Tab/index.tsx
+++ components/Tab/index.tsx
@@ -2,30 +2,31 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type Props = {};
 
 const Tab = (props: Props) => {
     const tabArr = [
-        ['글', ''],
-        ['채팅', 'chats'],
-        ['마이페이지', 'user'],
+        ['글', '/'],
+        ['채팅', '/chats'],
+        ['마이페이지', '/user'],
     ];
     const [clicked, setClicked] = useState('');
+    const router = useRouter();
+    const { pathname } = router;
 
-    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        const path = e.currentTarget.dataset.path;
-        if (path) setClicked(path);
-    };
+    useEffect(() => {
+        if (pathname) setClicked(pathname);
+    }, [pathname]);
 
     return (
         <>
             {tabArr.map((tab, i) => (
-                <Link key={i} href={`/${tab[1]}`}>
+                <Link key={i} href={`${tab[1]}`}>
                     <TabBox
                         data-path={tab[1]}
                         className={tab[1] === clicked ? 'clicked' : ''}
-                        onClick={handleClick}
                     >
                         {tab[0]}
                     </TabBox>
