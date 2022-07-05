d4a0a1fe14203752a4590c370c85f5dc4d1942e6
crucial-sub <jssub940@gmail.com>
2022-07-05 23:44:32 +0900

    [Feat] 채팅방 onOff값 off면 채팅 안보이도록 구현

--- components/Chat/Conversations.tsx
+++ components/Chat/Conversations.tsx
@@ -1,5 +1,11 @@
 import { dbService } from 'fbase';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import {
+    collection,
+    onSnapshot,
+    orderBy,
+    query,
+    where,
+} from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ItemTypes, RootState } from 'store';
@@ -20,29 +26,46 @@ export interface ChatContentType {
 const Conversations = (props: Props) => {
     const { items } = props;
     const [messages, setMessages] = useState<ChatContentType[]>([]);
-    const { itemDocId } = useSelector((state: RootState) => state.itemDoc);
     const { currentUser } = useSelector(
         (state: RootState) => state.currentUser,
     );
     const { docId } = useSelector((state: RootState) => state.docId);
+    const [isOn, setIsOn] = useState<boolean>();
 
     useEffect(() => {
-        if (docId) {
-            const chatRef = collection(dbService, 'chats', docId, 'messages');
-            const g = query(chatRef, orderBy('timestamp', 'asc'));
-            const unsubscribe = onSnapshot(g, (querySnapshot) => {
-                const mes = querySnapshot.docs.map((doc) => ({
-                    ...doc.data(),
-                    timeStamp: doc.data().timestamp?.toDate().getTime(),
-                }));
-                setMessages(mes);
-            });
-        }
+        const chatRef = collection(dbService, 'chats', docId, 'messages');
+        const g = query(chatRef, orderBy('timestamp', 'asc'));
+        const unsubscribe = onSnapshot(g, (querySnapshot) => {
+            const mes = querySnapshot.docs.map((doc) => ({
+                ...doc.data(),
+                timeStamp: doc.data().timestamp?.toDate().getTime(),
+            }));
+            setMessages(mes);
+        });
     }, [docId]);
-    // console.log(messages);
+    useEffect(() => {
+        const collectionRef = collection(dbService, 'chats');
+        const q = query(collectionRef, where('id', '==', docId));
+        const onOffCheck = onSnapshot(q, (querySnapshot) => {
+            if (querySnapshot.docs[0]) {
+                let chatOnOff;
+                if (currentUser.uid === items?.userId) {
+                    chatOnOff = querySnapshot.docs[0].data().onOff[1];
+                } else {
+                    chatOnOff = querySnapshot.docs[0].data().onOff[0];
+                }
+                if (chatOnOff === 'on') {
+                    setIsOn(true);
+                } else if (chatOnOff === 'off') {
+                    setIsOn(false);
+                }
+            }
+        });
+    }, [currentUser, messages]);
     return (
         <ContentBox>
             {messages &&
+                isOn &&
                 messages.map((a, i) => {
                     if (a.user === currentUser.email) {
                         return (
