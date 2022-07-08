faf27c494cdd042c27396dcc023923dc39121293
Space-Belt <dngurwn@gmail.com>
2022-07-08 20:39:59 +0900

    [Feat] TimeStamp 변환 유틸함수

---
+++ utils/dateFormat.ts
@@ -0,0 +1,16 @@
+export const getTimeDate = (time: Date) => {
+    const date = time.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+    return date;
+};
+
+export const getMonthDayTime = (time: Date) => {
+    const date = time.toLocaleDateString();
+    const hourMin = time.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+    return `${date} ${hourMin}`;
+};
