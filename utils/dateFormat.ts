73ac47af2e25cd54726b603ca70c4563552666ef
crucial-sub <jssub940@gmail.com>
2022-07-09 01:19:41 +0900

    [Fix] date 유틸 함수에서 time이 undefined일때 발생하는 오류 해결

--- utils/dateFormat.ts
+++ utils/dateFormat.ts
@@ -1,21 +1,27 @@
 export const getTimeDate = (time: Date) => {
-    const date = time.toLocaleTimeString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-    });
-    return date;
+    if (time) {
+        const date = time.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+        return date;
+    } else return;
 };
 
 export const getMonthDayTime = (time: Date) => {
-    const date = time.toLocaleDateString();
-    const hourMin = time.toLocaleTimeString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-    });
-    return `${date} ${hourMin}`;
+    if (time) {
+        const date = time.toLocaleDateString();
+        const hourMin = time.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+        return `${date} ${hourMin}`;
+    } else return;
 };
 
 export const getMonthDay = (time: Date) => {
-    const date = time.toLocaleDateString();
-    return date;
+    if (time) {
+        const date = time.toLocaleDateString();
+        return date;
+    } else return;
 };
