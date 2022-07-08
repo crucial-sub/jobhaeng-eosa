e640d5d15396cf4072e4151f3de2d9f7546ad62a
crucial-sub <jssub940@gmail.com>
2022-07-09 03:14:01 +0900

    [Feat] 잡행어사 해임 기능 구현

--- components/Chat/RequestCancel.tsx
+++ components/Chat/RequestCancel.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import { dbService } from 'fbase';
+import {
+    collection,
+    deleteDoc,
+    doc,
+    getDocs,
+    onSnapshot,
+    query,
+    updateDoc,
+    where,
+} from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { itemListAction, ItemTypes, RootState } from 'store';
 
-type Props = {};
+type Props = {
+    item: ItemTypes;
+    itemId: string;
+};
 
 const RequestCancel = (props: Props) => {
-    return <div>잡행어사 해임</div>;
+    const { item, itemId } = props;
+    const { docId } = useSelector((state: RootState) => state.docId);
+
+    const dispatch = useDispatch();
+
+    const handleClick = async (e: React.MouseEvent<HTMLDivElement>) => {
+        if (confirm('해임하시겠습니까?') && itemId) {
+            await updateDoc(doc(dbService, 'items', itemId), {
+                ongoing: false,
+            });
+            const newItem = { ...item, ongoing: false };
+            dispatch(itemListAction.update({ newItem, itemId }));
+            await updateDoc(doc(dbService, 'chats', docId), {
+                ongoing: false,
+            });
+        } else return;
+    };
+
+    return (
+        <RequestCancelBtn onClick={handleClick}>잡행어사 해임</RequestCancelBtn>
+    );
 };
+
+const RequestCancelBtn = styled.div`
+    cursor: pointer;
+`;
 
 export default RequestCancel;
