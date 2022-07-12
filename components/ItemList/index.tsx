140a564e275b8b75e806a6b7bb37dd80949a774f
crucial-sub <jssub940@gmail.com>
2022-07-13 00:22:07 +0900

    [Style] 보상금 최대금액 제한 설정 및 요청글 리스트 css 적용

--- components/ItemList/index.tsx
+++ components/ItemList/index.tsx
@@ -20,6 +20,7 @@ import {
     ItemTypes,
     RootState,
 } from 'store';
+import colors from 'styles/colors';
 import { getMonthDay, getMonthDayTime, numberCommas } from 'utils/dateFormat';
 import { getTownWithOutDist } from 'utils/fetcher';
 
@@ -52,12 +53,12 @@ const ItemList = (props: Props) => {
     useEffect(() => {
         if (!filterInfo.name) {
             const myTownItem = itemList.filter(
-                (item) => item.town === currentUser.town,
+                (item) => item.town === currentUser?.town,
             );
 
             dispatch(
                 filterAction.filter({
-                    name: currentUser.town,
+                    name: currentUser?.town,
                     code: '',
                     filteredItem: myTownItem,
                 }),
@@ -104,10 +105,12 @@ const ItemList = (props: Props) => {
                 filterInfo.filteredItem?.map((item) => (
                     <Link key={item.id} href={`/items/${item.id}`}>
                         <PostBox>
-                            <div>{item.ongoing ? '진행 중' : null}</div>
                             <div>{item.title}</div>
-                            <div>{item.location}</div>
+                            <div>{item.town}</div>
                             <div>{item.date}</div>
+                            <div>
+                                {item.ongoing ? <span>진행 중</span> : null}
+                            </div>
                             <div>{numberCommas(item.reward?.toString())}</div>
                         </PostBox>
                     </Link>
@@ -118,10 +121,63 @@ const ItemList = (props: Props) => {
 };
 
 const PostBox = styled.div`
-    display: flex;
-    margin: 20px;
+    display: grid;
+    margin: 15px 5px;
+    height: 5rem;
     cursor: pointer;
     background-color: #eeeeee;
+    grid-template-columns: 1.5fr 1.5fr 1fr 2fr;
+    grid-template-rows: 2fr 1fr 1fr;
+    border: none;
+    box-shadow: rgba(9, 30, 66, 0.25) 0px 1px 1px,
+        rgba(9, 30, 66, 0.13) 0px 0px 1px 1px;
+    border-radius: 10px;
+    padding: 1rem;
+    gap: 0.1rem;
+    color: ${colors.dark};
+
+    & div:nth-child(1) {
+        grid-column: 1 / 5;
+        grid-row: 1 / 2;
+        align-self: center;
+        font-size: 1.2rem;
+    }
+    & div:nth-child(2) {
+        grid-column: 1 / 3;
+        grid-row: 2 / 3;
+        align-self: center;
+        font-size: 0.8rem;
+        color: ${colors.lightDark};
+    }
+    & div:nth-child(3) {
+        grid-column: 1 / 3;
+        grid-row: 3 / 4;
+        align-self: center;
+        font-size: 0.8rem;
+        color: ${colors.lightDark};
+    }
+    & div:nth-child(4) {
+        grid-column: 3 / 4;
+        grid-row: 2 / 4;
+        justify-content: center;
+        align-self: center;
+        display: flex;
+        font-size: 0.8rem;
+        background-color: ${colors.lightDark};
+        border-radius: 5px;
+        & span {
+            max-width: 100%;
+            margin: 0.4rem 0.2rem;
+            color: ${colors.gold};
+        }
+    }
+    & div:nth-child(5) {
+        grid-column: 4 / 5;
+        grid-row: 2 / 4;
+        justify-self: center;
+        align-self: center;
+        font-size: 1.2rem;
+    }
 `;
 
 export default ItemList;
