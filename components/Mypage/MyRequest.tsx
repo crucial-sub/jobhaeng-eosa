d20173c3c98fa838469e13ca23648df65871c203
crucial-sub <jssub940@gmail.com>
2022-07-15 04:53:31 +0900

    [Feat] 마이페이지 요청내역 css 작업

--- components/Mypage/MyRequest.tsx
+++ components/Mypage/MyRequest.tsx
@@ -1,50 +1,41 @@
-import { dbService, firebaseInstance } from 'fbase';
-import {
-    collection,
-    doc,
-    DocumentData,
-    Firestore,
-    onSnapshot,
-    query,
-    QueryDocumentSnapshot,
-    where,
-} from 'firebase/firestore';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { ItemTypes, RootState } from 'store';
+import { RootState } from 'store';
 import styled from '@emotion/styled';
+import colors from 'styles/colors';
+import Link from 'next/link';
 
 type Props = {};
 
 const MyRequest = (props: Props) => {
-    const [datas, setDatas] = useState({});
     const { currentUser } = useSelector(
         (state: RootState) => state.currentUser,
     );
-    const collectionRef = collection(dbService, 'items');
     const { itemList } = useSelector((state: RootState) => state.itemList);
-    const wholeItems = [...itemList];
-    // const myRequests: ItemTypes[] = [{}];
-    const dataArr = wholeItems.filter(
+    const RequestList = itemList.filter(
         (data) => data.userId === currentUser?.uid,
     );
-    const count = dataArr.length;
 
     return (
-        <>
+        <div>
             <Information>요청 내역 </Information>
-            {dataArr.map((data, i) => {
-                return (
-                    <MydataList key={i}>
-                        <div>{data.title}</div>
-                        <div>{data.location}</div>
-                        <div>{data.reward}</div>
-                        <div>{data.date}</div>
-                    </MydataList>
-                );
-            })}
-        </>
+            <div>
+                {RequestList.length > 0
+                    ? RequestList.map((item) => {
+                          return (
+                              <Link href={`/items/${item.id}`} key={item.id}>
+                                  <ItemBox>
+                                      <div>{item.title}</div>
+                                      <div>{item.town}</div>
+                                      <div>{item.date}</div>
+                                      <div>{item.reward}</div>
+                                  </ItemBox>
+                              </Link>
+                          );
+                      })
+                    : '요청 내역이 없습니다.'}
+            </div>
+        </div>
     );
 };
 
@@ -55,17 +46,48 @@ const Information = styled.h1`
     padding: 15px 0;
 `;
 
-const MydataList = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    line-height: 40px;
-    text-align: center;
+const ItemBox = styled.div`
+    display: grid;
+    margin: 15px 5px;
+    height: 5rem;
+    cursor: pointer;
     background-color: #eeeeee;
-    & div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    grid-template-columns: 1.5fr 1.5fr 1fr 2fr;
+    grid-template-rows: 2fr 1fr 1fr;
+    border: none;
+    box-shadow: rgba(9, 30, 66, 0.25) 0px 1px 1px,
+        rgba(9, 30, 66, 0.13) 0px 0px 1px 1px;
+    border-radius: 10px;
+    padding: 1rem;
+    gap: 0.1rem;
+    color: ${colors.dark};
+
+    & div:nth-of-type(1) {
+        grid-column: 1 / 5;
+        grid-row: 1 / 2;
+        align-self: center;
+        font-size: 1.2rem;
+    }
+    & div:nth-of-type(2) {
+        grid-column: 1 / 3;
+        grid-row: 2 / 3;
+        align-self: center;
+        font-size: 0.8rem;
+        color: ${colors.lightDark};
+    }
+    & div:nth-of-type(3) {
+        grid-column: 1 / 3;
+        grid-row: 3 / 4;
+        align-self: center;
+        font-size: 0.8rem;
+        color: ${colors.lightDark};
+    }
+    & div:nth-of-type(4) {
+        grid-column: 4 / 5;
+        grid-row: 2 / 4;
+        justify-self: center;
+        align-self: center;
+        font-size: 1.2rem;
     }
 `;
 
