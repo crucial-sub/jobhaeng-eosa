f879e717a0aa94c4fd0bf4bb912a8c3ee3cbe29e
crucial-sub <jssub940@gmail.com>
2022-07-16 00:14:45 +0900

    [Style] 요청글 내용 css 작업

--- components/Request/RequestDetail.tsx
+++ components/Request/RequestDetail.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { ItemTypes, requestAction } from 'store';
+import colors from 'styles/colors';
 
 type Props = {
     request: ItemTypes;
@@ -18,15 +19,26 @@ const RequestDetail = (props: Props) => {
         );
     };
     return (
-        <>
-            <Label>잡행 내용</Label>
-            <TextArea required onChange={handleChange}></TextArea>
-        </>
+        <DetailBox>
+            <TextArea
+                required
+                onChange={handleChange}
+                placeholder="잡행어사에게 요청할 내용을 작성해주세요."
+            ></TextArea>
+        </DetailBox>
     );
 };
-
-const Label = styled.label``;
-
-const TextArea = styled.textarea``;
+const DetailBox = styled.div`
+    > textarea {
+        min-height: 9rem;
+    }
+`;
+const TextArea = styled.textarea`
+    padding: 1rem 0;
+    border: none;
+    resize: none;
+    background-color: ${colors.white};
+    font-size: 1rem;
+`;
 
 export default RequestDetail;
