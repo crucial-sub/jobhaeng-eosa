8cff0270617c1ca1ad467d18d983aeff8b138458
crucial-sub <jssub940@gmail.com>
2022-07-29 07:09:59 +0900

    [Feat] 요청글 작성 페이지에서 이메일 미인증 유저나 유저 정보 미작성 유저는 접근제한

--- pages/request.tsx
+++ pages/request.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import Request from 'components/Request';
 import colors from 'styles/colors';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+import { useRouter } from 'next/router';
 
 type Props = {};
 
 const request = (props: Props) => {
+    const { currentUser } = useSelector(
+        (state: RootState) => state.currentUser,
+    );
+    const router = useRouter();
+    useEffect(() => {
+        if (currentUser.email) {
+            if (!currentUser.emailVerified) {
+                if (
+                    confirm(
+                        '이메일 인증 후 내 동네와 닉네임을 설정해야만 요청글 작성이 가능합니다! 이메일 인증 페이지로 이동하시겠습니까?',
+                    )
+                )
+                    router.push('/validate');
+                else router.back();
+            } else if (!currentUser.town || !currentUser.nickName) {
+                if (
+                    confirm(
+                        '내 동네와 닉네임을 설정해야만 요청글 작성이 가능합니다! 프로필 수정 페이지로 이동하시겠습니까?',
+                    )
+                )
+                    router.push('/user/edit');
+                else router.back();
+            }
+        }
+    }, [currentUser]);
+
     return (
-        <RequestWrapper>
-            <Request />
-        </RequestWrapper>
+        <>
+            {currentUser.emailVerified &&
+                currentUser.nickName &&
+                currentUser.town &&
+                currentUser.email && (
+                    <RequestWrapper>
+                        <Request />
+                    </RequestWrapper>
+                )}
+        </>
     );
 };
 
