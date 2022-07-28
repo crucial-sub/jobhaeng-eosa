d9c72ac50ee13a1ed6d3e5f14841330317694985
crucial-sub <jssub940@gmail.com>
2022-07-29 06:12:56 +0900

    [Feat] 유저 수정 페이지에 이메일 미인증 유저는 접근 제한

--- pages/user/edit.tsx
+++ pages/user/edit.tsx
@@ -1,9 +1,25 @@
 import EditUser from 'components/EditUser';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
 
 type Props = {};
 
 const EditPage = (props: Props) => {
+    const { currentUser } = useSelector(
+        (state: RootState) => state.currentUser,
+    );
+    const router = useRouter();
+    if (!currentUser.emailVerified) {
+        if (
+            confirm(
+                '이메일 인증 유저만 프로필 수정이 가능합니다! 이메일 인증 페이지로 이동하시겠습니까?',
+            )
+        ) {
+            return router.push('/validate');
+        } else return router.back();
+    }
     return <EditUser />;
 };
 
