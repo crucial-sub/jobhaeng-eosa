1dc2eb3d5c3af5a2ab9ff9ccd2ee6be498c933a2
crucial-sub <jssub940@gmail.com>
2022-07-29 07:08:05 +0900

    [Fix] 채팅 버튼과 요청글 작성 버튼 클릭 조건에 유저 닉네임도 추가

--- components/Item/ChatButton.tsx
+++ components/Item/ChatButton.tsx
@@ -34,7 +34,7 @@ const ChatButton = (props: Props) => {
             confirm(
                 '이메일 인증 후 내 동네와 닉네임을 설정해야만 채팅이 가능합니다! 이메일 인증 페이지로 이동하시겠습니까?',
             ) && router.push('/validate');
-        } else if (!currentUser.town) {
+        } else if (!currentUser.town || !currentUser.nickName) {
             confirm(
                 '내 동네와 닉네임을 설정해야만 요청글 작성이 가능합니다! 프로필 수정 페이지로 이동하시겠습니까?',
             ) && router.push('/user/edit');
