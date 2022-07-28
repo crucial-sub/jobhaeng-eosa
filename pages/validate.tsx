fa76e8c7c4ea097b55174a3290f4888dde49e7c3
crucial-sub <jssub940@gmail.com>
2022-07-29 06:28:37 +0900

    [Feat] 이메일 인증페이지에서 이미 인증이 완료된 유저는 접근 제한

--- pages/validate.tsx
+++ pages/validate.tsx
@@ -1,10 +1,24 @@
 import EmailValidate from 'components/Mypage/EmailValidate';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
 
 type Props = {};
 
 const validate = (props: Props) => {
-    return <EmailValidate />;
+    const { currentUser } = useSelector(
+        (state: RootState) => state.currentUser,
+    );
+    const router = useRouter();
+
+    useEffect(() => {
+        if (currentUser.emailVerified) {
+            alert('이미 이메일 인증이 완료되었습니다!');
+            router.push('/user');
+        }
+    }, [currentUser.emailVerified]);
+    return <>{!currentUser.emailVerified && <EmailValidate />}</>;
 };
 
 export default validate;
