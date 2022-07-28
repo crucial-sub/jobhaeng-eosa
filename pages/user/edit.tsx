4098b9b62239412303d0ebd12cfffee17afc559f
crucial-sub <jssub940@gmail.com>
2022-07-29 07:12:21 +0900

    [Fix] 이메일 미인증 유저 접근제한하는 로직 수정

--- pages/user/edit.tsx
+++ pages/user/edit.tsx
@@ -1,6 +1,6 @@
 import EditUser from 'components/EditUser';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store';
 
@@ -11,15 +11,18 @@ const EditPage = (props: Props) => {
         (state: RootState) => state.currentUser,
     );
     const router = useRouter();
-    if (!currentUser.emailVerified) {
-        if (
-            confirm(
-                '이메일 인증 유저만 프로필 수정이 가능합니다! 이메일 인증 페이지로 이동하시겠습니까?',
-            )
-        ) {
-            return router.push('/validate');
-        } else return router.back();
-    }
+
+    useEffect(() => {
+        if (!currentUser.emailVerified && currentUser.email) {
+            if (
+                confirm(
+                    '이메일 인증 유저만 프로필 수정이 가능합니다! 이메일 인증 페이지로 이동하시겠습니까?',
+                )
+            ) {
+                router.push('/validate');
+            } else router.back();
+        }
+    }, [currentUser]);
     return <EditUser />;
 };
 
